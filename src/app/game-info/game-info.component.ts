08a3e5ef6e70145ebdd01dd292edc95d403f0e13
navjotsingh96 <navjot.singh96@web.de>
2022-04-25 14:07:15 +0200

    added rules and showen too

--- src/app/game-info/game-info.component.ts
+++ src/app/game-info/game-info.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-game-info',
   templateUrl: './game-info.component.html',
   styleUrls: ['./game-info.component.scss']
 })
-export class GameInfoComponent implements OnInit {
+export class GameInfoComponent implements OnInit, OnChanges {
+
+  cardAction = [
+    { title: 'Waterfall', description: 'Everyone has to start drinking at the same time. As soon as player 1 stops drinking, player 2 may stop drinking. Player 3 may stop as soon as player 2 stops drinking, and so on.' },
+    { title: 'You', description: 'You decide who drinks' },
+    { title: 'Me', description: 'Congrats! Drink a shot!' },
+    { title: 'Category', description: 'Come up with a category (e.g. Colors). Each player must enumerate one item from the category.' },
+    { title: 'Bust a jive', description: 'Player 1 makes a dance move. Player 2 repeats the dance move and adds a second one. ' },
+    { title: 'Chicks', description: 'All girls drink.' },
+    { title: 'Heaven', description: 'Put your hands up! The last player drinks!' },
+    { title: 'Mate', description: 'Pick a mate. Your mate must always drink when you drink and the other way around.' },
+    { title: 'Thumbmaster', description: 'Person who drew card is now thumbmaster. They can put there thumb on the table at any time through the rest of the game or until another person draws a 5 and becomes thumbmaster. Everyone else has to then put there thumb on the table; the last one to do so drinks' },
+    { title: 'Men', description: 'All men drink.' },
+    { title: 'Quizmaster', description: 'Ask somebody a difficult question.' },
+    { title: 'Never have i ever...', description: 'Say something you never did. Everyone who did it has to drink.' },
+    { title: 'Rule', description: 'Make a rule. Everyone needs to drink when he breaks the rule.' },
+  ];
+
+  title = '';
+  description = '';
+  @Input() card: string;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // wie der name ist, wird immer aufgeführt wenn hier was geändert wird
+  ngOnChanges(): void {
+    // nur dann aufführen wenn die Karte gezogen wird.
+    if (this.card) {
+      // wird die name gesplitet von _ . sinst voll name ist z.B. Spiede_12 dann wird ist in ein Array verwandet, dann hoken wir 1 Array.
+      let cardNumber = +this.card.split('_')[1];
+
+      // CardAction ist oben definiert. Cardnumber -1, weil wir nicht von 0 anfanngen sonst von 1. 
+      this.title = this.cardAction[cardNumber - 1].title;
+      this.description = this.cardAction[cardNumber - 1].description;
+    }
   }
 
 }
